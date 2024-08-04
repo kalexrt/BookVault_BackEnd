23235d150ba96908b2dba11ab7a89bccd92409f3
Kalash Shrestha <69349296+kalexrt@users.noreply.github.com>
2024-08-04 23:07:40 +0545

    fix total review not updating bug

--- src/service/review.service.ts
+++ src/service/review.service.ts
@@ -6,15 +6,28 @@ import loggerWithNameSpace from "../utils/logger";
 const logger = loggerWithNameSpace("ReviewService");
 
 export async function getToReiviewBooks(userId: string) {
-    logger.info("Called getToReiviewBooks");
-    const data = ReviewModel.getToReiviewBooks(userId);
-    return data;
+  logger.info("Called getToReiviewBooks");
+  const data = ReviewModel.getToReiviewBooks(userId);
+  return data;
 }
 
-export async function addReviewReturnAvgRating(userId: string, borrowId: string, rating: string) {
-    logger.info("Called addReviewReturnAvgRating");
-    const bookId = await BorrowModel.getBookIdByBorrowId(borrowId);
-    const newAvgRating = await ReviewModel.addReviewReturnAvgRating(+userId, +bookId, +rating); 
-    await BorrowModel.updateIsReviewed(borrowId); // update is_reviewed to true in users_issued_books
-    await BookModel.updateBookById(bookId, { rating: +newAvgRating },userId); // update book rating in books
+export async function addReviewReturnAvgRating(
+  userId: string,
+  borrowId: string,
+  rating: string
+) {
+  logger.info("Called addReviewReturnAvgRating");
+  const bookId = await BorrowModel.getBookIdByBorrowId(borrowId);
+  const book = await BookModel.getBookById(bookId);
+  const newAvgRating = await ReviewModel.addReviewReturnAvgRating(
+    +userId,
+    +bookId,
+    +rating
+  );
+  await BorrowModel.updateIsReviewed(borrowId); // update is_reviewed to true in users_issued_books
+  await BookModel.updateBookById(
+    bookId,
+    { rating: +newAvgRating, totalReviews: book.total_reviews + 1 },
+    userId
+  ); // update book rating in books
 }
